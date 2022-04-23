90b64c0c55881ef6ef84dc185f913d64431afbe4
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-22 21:21:28 -0500

    entered starter code plus some stuff I already started coding

---
+++ js/quiz.js
@@ -0,0 +1,46 @@
+/* 
+1. Store correct answers
+- When quiz begins, no answers are correct
+*/
+let correctAnswers = false
+
+// 2. Store the rank of a player
+let rank = correctAnswers 
+
+// 3. Select the <main> HTML element
+const tallyAndRank = ` `You got ${} out of 5 questions correct.
+
+/*
+4. Ask at least 5 questions
+- Store each answer in a variable
+- Keep track of the number of correct answers using a variable
+- addition assignement operator
+*/
+
+correctAnswers += 
+let question1 = 
+let question2 =
+let question3 =
+let question4 =
+let question5 =
+/*
+5. Rank player based on number of correct answers
+- 5 correct = Gold
+- 3-4 correct = Silver
+- 1-2 correct = Bronze
+- 0 correct = No crown
+*/
+if ( question1 = true ) {
+    correctAnswers = true
+} else if () {
+
+} else if () {
+
+} else if () {
+
+} else {
+
+}
+
+// 6. Output results to the <main> element
+document.querySelector('main').innerHTML = tallyAndRank;
