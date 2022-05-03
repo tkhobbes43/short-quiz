8ee0d0825c7d0ff655b415c87bde8eb962ca0ac6
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-02 22:25:30 -0500

    conditional statement for rank set up

--- js/quiz.js
+++ js/quiz.js
@@ -21,20 +21,20 @@ const answer1 = prompt("Name a programming language that is also a gem.");
 if (answer1.toUpperCase() === 'RUBY') {
     correct += 1;
 }
-const answer2 = prompt("Name a programming language that is also a gem.");
-if (answer1.toUpperCase() === 'RUBY') {
+const answer2 = prompt("Name a programming language that is also a snake.");
+if (answer1.toUpperCase() === 'PYTHON') {
     correct += 1;
 }
-const answer3 = prompt("Name a programming language that is also a gem.");
-if (answer1.toUpperCase() === 'RUBY') {
+const answer3 = prompt("What language do you use to style web pages?");
+if (answer1.toUpperCase() === 'CSS') {
     correct += 1;
 }
-const answer4 = prompt("Name a programming language that is also a gem.");
-if (answer1.toUpperCase() === 'RUBY') {
+const answer4 = prompt("What language do you use to build the structure of web pages?");
+if (answer1.toUpperCase() === 'HTML') {
     correct += 1;
 }
-const answer5 = prompt("Name a programming language that is also a gem.");
-if (answer1.toUpperCase() === 'RUBY') {
+const answer5 = prompt("What language do you use to add interactivity to a web page?");
+if (answer1.toUpperCase() === 'JAVASCRIPT') {
     correct += 1;
 }
 /*
@@ -44,16 +44,14 @@ if (answer1.toUpperCase() === 'RUBY') {
 - 1-2 correct = Bronze
 - 0 correct = No crown
 */
-if ( question1 = true ) {
-    correctAnswers = true
-} else if () {
-
-} else if () {
-
-} else if () {
-
+if ( correct === 5 ) {
+    rank = "Gold";
+} else if (correct >= 3 ) {
+    rank = "Silver";
+} else if (correct >= 2) {
+    rank = "Bronze";
 } else {
-
+    rank = "None :(";
 }
 
 // 6. Output results to the <main> element
