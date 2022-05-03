bda2d8605019dc847436faceb4ed2bb73800aca2
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-02 22:51:32 -0500

    used console logs to find out why I kept gettingr ank of bronze even though I was answering all the questions correctly. in the end it was because i wasn't using correct const for each question in the conditional

--- js/quiz.js
+++ js/quiz.js
@@ -5,7 +5,7 @@
 let correct = 0;
 
 // 2. Store the rank of a player
-let rank = ""; 
+let rank;
 
 // 3. Select the <main> HTML element
 const main = document.querySelector(`main`);
@@ -20,23 +20,38 @@ const main = document.querySelector(`main`);
 const answer1 = prompt("Name a programming language that is also a gem.");
 if (answer1.toUpperCase() === 'RUBY') {
     correct += 1;
+    console.log(answer1);
+    console.log (correct);
 }
+
 const answer2 = prompt("Name a programming language that is also a snake.");
-if (answer1.toUpperCase() === 'PYTHON') {
+if (answer2.toUpperCase() === 'PYTHON') {
     correct += 1;
+    console.log(answer2);
+    console.log (correct);
 }
+
 const answer3 = prompt("What language do you use to style web pages?");
-if (answer1.toUpperCase() === 'CSS') {
+if (answer3.toUpperCase() === 'CSS') {
     correct += 1;
+    console.log(answer3);
+    console.log (correct);
 }
+
 const answer4 = prompt("What language do you use to build the structure of web pages?");
-if (answer1.toUpperCase() === 'HTML') {
+if (answer4.toUpperCase() === 'HTML') {
     correct += 1;
+    console.log(answer4);
+    console.log (correct);
 }
+
 const answer5 = prompt("What language do you use to add interactivity to a web page?");
-if (answer1.toUpperCase() === 'JAVASCRIPT') {
+if (answer5.toUpperCase() === 'JAVASCRIPT') {
     correct += 1;
+    console.log(answer5);
+    console.log (correct);
 }
+
 /*
 5. Rank player based on number of correct answers
 - 5 correct = Gold
@@ -48,11 +63,14 @@ if ( correct === 5 ) {
     rank = "Gold";
 } else if (correct >= 3 ) {
     rank = "Silver";
-} else if (correct >= 2) {
+} else if (correct >= 1) {
     rank = "Bronze";
 } else {
     rank = "None :(";
 }
 
 // 6. Output results to the <main> element
-document.querySelector('main').innerHTML = tallyAndRank;
+main.innerHTML = `
+  <h2>You got ${correct} out of 5 questions correct.</h2>
+  <p>Crown earned: <strong>${rank}</strong></p>
+`;
